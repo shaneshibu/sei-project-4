c13a3a23f9c6163581efaa45426e5c59d6047984
Shane Shibu <shane.shibu.09@gmail.com>
2019-07-26 12:48:20 +0100

    created base model and schema mixin

--- models/image.py
+++ models/image.py
@@ -1,15 +1,15 @@
 from app import db, ma
+from models.base import BaseModel, BaseSchema
 
-class Image(db.Model):
+class Image(db.Model, BaseModel):
 
     __tablename__ = 'images'
 
-    id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.Text, nullable=False)
     #user_id
     #post_ids
 
-class ImageSchema(ma.ModelSchema):
+class ImageSchema(ma.ModelSchema, BaseSchema):
 
     class Meta:
         model = Image
