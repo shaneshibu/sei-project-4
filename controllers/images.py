fcecf646cb327e9e481f6264957062a6f295f636
Shane Shibu <shane.shibu.09@gmail.com>
2019-07-26 15:17:24 +0100

    created add image route

--- controllers/images.py
+++ controllers/images.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from models.image import Image, ImageSchema
 
 api = Blueprint('images', __name__)
@@ -9,3 +9,15 @@ image_schema = ImageSchema()
 def index():
     images = Image.query.all()
     return image_schema.jsonify(images, many=True), 200
+
+@api.route('/images', methods=['POST'])
+def add():
+    try:
+        data = request.get_json()
+    except:
+        return {'message': 'That is not a valid JSON Object'}, 422
+    image, errors = image_schema.load(data)
+    if errors:
+        return errors, 422
+    image.save()
+    return image_schema.jsonify(image), 201
