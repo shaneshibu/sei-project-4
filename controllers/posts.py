82324531bd6f87a5d72b8600b45c6a1b54913d3c
Shane Shibu <shane.shibu.09@gmail.com>
2019-07-28 18:32:05 +0100

    added create post route and added length validation to post title

--- controllers/posts.py
+++ controllers/posts.py
@@ -1,5 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, request, g
 from models.post import Post, PostSchema
+from lib.secure_route import secure_route
 
 api = Blueprint('posts', __name__)
 
@@ -9,3 +10,17 @@ post_schema = PostSchema()
 def index():
     posts = Post.query.all()
     return post_schema.jsonify(posts, many=True), 200
+
+@api.route('/posts', methods=['POST'])
+@secure_route
+def create():
+    try:
+        data = request.get_json()
+    except:
+        return {'message': 'That is not a valid JSON Object'}, 422
+    post, errors = post_schema.load(data)
+    if errors:
+        return errors, 422
+    post.creator = g.current_user
+    post.save()
+    return post_schema.jsonify(post), 201
--- models/post.py
+++ models/post.py
@@ -1,9 +1,9 @@
-from marshmallow import fields
+from marshmallow import fields, validate
 from app import db, ma
 from .base import BaseModel, BaseSchema
 # pylint: disable=W0611
 from .user import User
-from .image import Image
+# from .image import Image
 
 class Post(db.Model, BaseModel):
 
@@ -19,7 +19,8 @@ class PostSchema(ma.ModelSchema, BaseSchema):
         model = Post
 
     # images = fields.Nested('ImageSchema', many=True, only=('url'))
-    post_entries = fields.Nested('EntrySchema', many=True, exclude=('created_at', 'updated_at', 'id'))
+    title = fields.String(required=True, validate=validate.Length(min=1, max=50))
+    post_entries = fields.Nested('EntrySchema', many=True, exclude=('created_at', 'updated_at', 'id', 'post'))
 
 class Entry(db.Model, BaseModel):
 
@@ -30,9 +31,11 @@ class Entry(db.Model, BaseModel):
     position = db.Column(db.Integer, default=0)
     caption = db.Column(db.String(30))
     post = db.relationship('Post', backref='post_entries')
-    image = db.relationship('Image', backref='image_entries')
+    # image = db.relationship('Image', backref='image_posts')
 
 class EntrySchema(ma.ModelSchema):
 
     class Meta:
         model = Entry
+
+    image_url = fields.Nested('ImageSchema', only='url')
