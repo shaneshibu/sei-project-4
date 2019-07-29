588ebe9eafd6223335aa27b01ac78f11f5ca8e14
Shane Shibu <shane.shibu.09@gmail.com>
2019-07-29 08:21:57 +0100

    created home page component

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
-console.log('Hello')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import 'bulma'
+
+
+import Home from './components/common/Home'
+import './style.scss'
+
+const App = () => (
+  <BrowserRouter>
+    <main>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </main>
+  </BrowserRouter>
+)
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
---
+++ src/components/common/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+class Home extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {}
+
+  }
+
+  render() {
+    return (
+      <section className="section">
+        <div className="container">
+          <h1 className="title">Home Page</h1>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Home
