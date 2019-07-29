0d98c84c1fac33fbbf05f1fe849c762588a29e25
Shane Shibu <shane.shibu.09@gmail.com>
2019-07-29 16:40:56 +0100

    get images from api, generates posts for each user

--- seeds.py
+++ seeds.py
@@ -1,3 +1,4 @@
+from random import randrange
 import requests
 from app import app, db
 from models.user import UserSchema
@@ -7,14 +8,26 @@ from config.environment import pixabay_api_key
 
 # get random users from randomuser.me
 response = requests.get(
-'https://randomuser.me/api/',
-params={
-'results': 50,
-'nat': 'gb',
-'inc': 'name,email,login,picture',
-'noinfo': True
+    'https://randomuser.me/api/',
+    params={
+        'results': 10,
+        'nat': 'gb',
+        'inc': 'name,email,login,picture',
+        'noinfo': True
 })
-users = response.json()['results']
+api_users = response.json()['results']
+
+# get images from pixabay
+response = requests.get(
+    'https://pixabay.com/api/',
+    params={
+        'key': pixabay_api_key,
+        'editors_choice': True,
+        'page': 1,
+        'per_page': 50
+    }
+)
+api_images = response.json()['hits']
 
 user_schema = UserSchema()
 
@@ -22,8 +35,9 @@ with app.app_context():
     db.drop_all()
     db.create_all()
 
-
-    for user in users:
+    # create new users from api users
+    users = []
+    for user in api_users:
         user, errors = user_schema.load({
             'username': user['login']['username'],
             'email': user['email'],
@@ -32,7 +46,8 @@ with app.app_context():
         })
         if errors:
             raise Exception(errors)
-        db.session.add(user)
+        user.save()
+        users.append(user)
 
     user_1, errors = user_schema.load({
         'username':'user1',
@@ -51,6 +66,15 @@ with app.app_context():
         })
     if errors:
         raise Exception(errors)
+
+    # create new images from api images
+    images = []
+    for image in api_images:
+        image = Image(
+            url=image['webformatURL'],
+            uploader=users[randrange(len(users))]
+        )
+        images.append(image)
 
     image_1 = Image(
         url='https://pixabay.com/get/52e3d6414853a914f6d1867dda6d49214b6ac3e456577240722c78d390/shadow-4332215_1920.jpg',
@@ -72,26 +96,21 @@ with app.app_context():
         uploader=user_2
     )
     # image_5 = Image(url='https://pixabay.com/get/52e3d14b4a53a914f6d1867dda6d49214b6ac3e456577240722c7ed69f/moto-4348015_1920.jpg')
-    # seed.append(image_5)
     # image_6 = Image(url='https://pixabay.com/get/52e3d6454a56a514f6d1867dda6d49214b6ac3e456577240722c7cd493/berry-breakfast-4336049_1920.jpg')
-    # seed.append(image_6)
     # image_7 = Image(url='https://pixabay.com/get/52e3d14b4851af14f6d1867dda6d49214b6ac3e456577240722c7cd691/nettle-4348233_1920.jpg')
-    # seed.append(image_7)
     # image_8 = Image(url='https://pixabay.com/get/52e3d0424854aa14f6d1867dda6d49214b6ac3e456577240722c73d590/agriculture-4351266_1920.jpg')
-    # seed.append(image_8)
     # image_9 = Image(url='https://pixabay.com/get/52e3d0454852ae14f6d1867dda6d49214b6ac3e456577240722c72d695/firenze-4356202_1920.jpg')
-    # seed.append(image_9)
 
     entry_1 = Entry(
-    image=image_1,
-    caption='Caption 1',
-    position=0
+        image=image_1,
+        caption='Caption 1',
+        position=0
     )
 
     entry_2 = Entry(
-    image=image_2,
-    caption='Caption 2',
-    position=1
+        image=image_2,
+        caption='Caption 2',
+        position=1
     )
 
     post_1 = Post(
@@ -101,7 +120,6 @@ with app.app_context():
         entry_1,
         entry_2
     ])
-
 
     post_2 = Post(
         title='Post 2',
@@ -117,14 +135,37 @@ with app.app_context():
 
     ])
 
-    # db.session.add_all(users)
+    # for each user, add all uploaded images to posts, each post no more than 5 entries long
+    for user in users:
+        if user.uploaded_images:
+            i = 0
+            while i < len(user.uploaded_images):
+                entries = []
+                pos = 0
+                while pos < 5 and i < len(user.uploaded_images):
+                    entry = Entry(
+                        image=user.uploaded_images[i],
+                        position=pos
+                    )
+                    entries.append(entry)
+                    pos += 1
+                    i += 1
+                post = Post(
+                    title='New Post',
+                    creator=user,
+                    post_entries=entries
+                )
+                post.save()
+                print(post.creator, post.post_entries)
+
     db.session.add_all([
-            image_1,
-            image_2,
-            image_3,
-            image_4,
-            entry_1,
-            entry_2,
+            # image_1,
+            # image_2,
+            # image_3,
+            # image_4,
             post_1
-    ])
+        ]
+        # + users
+        # + images
+    )
     db.session.commit()
