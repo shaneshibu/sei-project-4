9ac3e8dd387ba8fac67ae3ebabd424b5c5019466
Shane Shibu <shane.shibu.09@gmail.com>
2019-07-29 20:47:11 +0100

    posts are now displayed on page using bulma columns

--- src/components/posts/IndexPosts.js
+++ src/components/posts/IndexPosts.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 class IndexPosts extends React.Component {
   constructor() {
@@ -8,10 +9,27 @@ class IndexPosts extends React.Component {
 
   }
 
+  componentDidMount(){
+    axios.get('/api/posts')
+      .then(res => this.setState({ posts: res.data }))
+      .catch(err => console.log(err))
+  }
+
   render() {
+    this.state.posts && console.log(this.state.posts[0].post_entries[0].image.url)
     return (
       <div className="container">
         <h1 className="title">Index Posts</h1>
+        <div className="columns is-multiline">
+          {this.state.posts && this.state.posts.map(post => (
+            <div key={post.id} className="column is-one-fifth">
+              <p>{post.title}</p>
+              <figure className="image">
+                {post.post_entries[0] && <img src={post.post_entries[0].image.url} alt={post.title}/>}
+              </figure>
+            </div>
+          ))}
+        </div>
       </div>
     )
   }
