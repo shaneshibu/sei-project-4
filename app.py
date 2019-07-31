db55e1b2577de5554f5f93266eeb5fbfee2a1a39
Shane Shibu <shane.shibu.09@gmail.com>
2019-07-31 15:09:40 +0100

    made changes to deploy to heroku

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
-from config.environment import db_url
+from config.environment import db_uri
 
 app = Flask(__name__, static_folder='dist')
-app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
--- config/environment.py
+++ config/environment.py
@@ -1,6 +1,6 @@
 import os
 
-db_url = os.getenv('DATABASE_URI', 'postgres://localhost:5432/imagebored')
+db_uri = os.getenv('DATABASE_URL', 'postgres://localhost:5432/imagebored')
 
 secret = os.getenv('SECRET', 'Shhh.. It\'s a secret')
 
